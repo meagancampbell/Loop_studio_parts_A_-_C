a20b3ea462791dd1a1fe87439e8236d3657a3938
Meagan Campbell <meagancampbell@users.noreply.github.com>
2021-06-13 18:24:36 +0000

    -

--- index.js
+++ index.js
@@ -1,33 +1,39 @@
+const input = require('readline-sync');
+
 //Part A #1: Initialize variables to store the following arrays. Remember to use descriptive names.
 
-let protein = ['chicken', 'pork', 'tofu', 'beef', 'fish', 'beans'];
-let grains = ['rice', 'pasta', 'corn', 'potato', 'quinoa', 'crackers'];
-let vegetables = ['peas', 'green beans', 'kale','edamame', 'broccoli', 'asparagus'];
-let beverage = ['juice', 'milk', 'water', 'soy milk', 'soda', 'tea'];
-let dessert = ['apple', 'banana', 'more kale','ice cream', 'chocolate', 'kiwi'];
+let astroNum = 0;
 
-let pantry = [protein, grains, vegetables, beverage, dessert];
-
-//Part A #2: Construct a for loop that assembles a meal for each of 6 astronauts.
-let astro = [];
-
-
-for (let i = 0; i < 7; i++){
-  let meal = [protein[i], grains[i], vegetables[i], beverage[i], dessert[i]];
-    console.log(meal);
+while (astroNum > 6 || astroNum < 0) {
+  mealCount = input.question("How many meals should I prepare?")
 }
 
+let pantry = [['chicken', 'pork', 'tofu', 'beef', 'fish', 'beans'],
+['rice', 'pasta', 'corn', 'potato', 'quinoa', 'crackers'],
+['peas', 'green beans', 'kale','edamame', 'broccoli', 'asparagus'],
+['juice', 'milk', 'water', 'soy milk', 'soda', 'tea'],
+['apple', 'banana', 'more kale','ice cream', 'chocolate', 'kiwi']]
+
+let i = 0;
+//Part A #2: Construct a for loop that assembles a meal for each of 6 astronauts.
+// let astro = [];
+
+
+// for (let i = 0; i < 7; i++){
+//   let meal = [protein[i], grains[i], vegetables[i], beverage[i], dessert[i]];
+//     console.log(meal);
+// }
 
 //Part C #5: Using a while loop, ask the user to select the number of meals to assemble.  Validate the input to make sure it is an integer from 1 - 6.
 
 
-let astroMeal = 0;
+let output = input.question("How many meals should I prepare?"); 
 
-
-
-// expected output: 3
-
-
+while (i < astroNum){
+  let output = `Meals: ${pantry[0][i]}, ${pantry[1][i]}, ${pantry[2][i]}, ${[3][i]}, ${[4][i]}`
+}
+console.log(output);
+i++;
 
 
 //Skill boosts: 
