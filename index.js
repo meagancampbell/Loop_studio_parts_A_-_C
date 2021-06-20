66d2f28ca9169b9a2e35ee0781c9efab7540ca59
Meagan Campbell <meagancampbell@users.noreply.github.com>
2021-06-20 20:20:44 +0000

    I dont know what I'm doing

--- index.js
+++ index.js
@@ -1,44 +1,31 @@
 const input = require('readline-sync');
+let output = input.question("How many meals should I prepare?"); 
 
 //Part A #1: Initialize variables to store the following arrays. Remember to use descriptive names.
-let astroNum = 0;
+let astroNum;
 
-while (astroNum > 6 || astroNum < 0) {
-  mealCount = input.question("How many meals should I prepare?")
-}
 let protein = ['chicken', 'pork', 'tofu', 'beef', 'fish', 'beans'];
 let grain = ['rice', 'pasta', 'corn', 'potato', 'quinoa', 'crackers'];
 let vegetable = ['peas', 'green beans', 'kale','edamame', 'broccoli', 'asparagus'];
 let beverage = ['juice', 'milk', 'water', 'soy milk', 'soda', 'tea'];
 let dessert = ['apple', 'banana', 'more kale','ice cream', 'chocolate', 'kiwi'];
 
-// let pantry = [['chicken', 'pork', 'tofu', 'beef', 'fish', 'beans'],
-// ['rice', 'pasta', 'corn', 'potato', 'quinoa', 'crackers'],
-// ['peas', 'green beans', 'kale','edamame', 'broccoli', 'asparagus'],
-// ['juice', 'milk', 'water', 'soy milk', 'soda', 'tea'],
-// ['apple', 'banana', 'more kale','ice cream', 'chocolate', 'kiwi']]
-
 
 //Part A #2: Construct a for loop that assembles a meal for each of 6 astronauts.
-// let astroMeal = [];
+const pantry = [protein, grain, vegetable, beverage, dessert];
 
-// for (let i = 0; i < 6; i++){
-//   let meal = [protein[i], grain[i], vegetable[i], beverage[i], dessert[i]];
-    
-// }
-
-
+for (i = 0; i < 6; i++){
+  console.log(`Astronaut ${i+1} gets a meal of ${protein[i]}, ${grain[i]}, ${vegetable[i]}, ${beverage[i]}, and ${dessert[i]}.`)
+}
+console.table(pantry);
 
 //Part C #5: Using a while loop, ask the user to select the number of meals to assemble.  Validate the input to make sure it is an integer from 1 - 6.
+const mealCount = input.question("How many meals should I assemble?");
 
+while (num < 1 || num > 6 || isNaN(num)){
+  num = input.question('Invalid input. Please enter a number from 1 to 6: ');
+}
 
-// let output = input.question("How many meals should I prepare?"); 
-
-// while (i < astroNum){
-//   let output = `Meals: ${pantry[0][i]}, ${pantry[1][i]}, ${pantry[2][i]}, ${[3][i]}, ${[4][i]}`
-// }
-// console.log(output);
-// i++;
 
 
 //Skill boosts: 
